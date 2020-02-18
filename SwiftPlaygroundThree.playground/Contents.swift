83fcb21967b628518b37245ab1c452e1bb25d530
Charles Ombao <combao@hawaii.edu>
2020-02-18 10:20:20 -1000

    Completed parts 1-7 and complete Problem Set 1 & 2

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -1,3 +1,40 @@
 import UIKit
+//SwiftPlayghroundThree 02.18.20
+//Part 2
 
-var str = "Hello, playground"
+//var fruitNames: [String]
+
+//Part 3
+
+var fruitNames = ["Apple", "Mango", "Peach"]
+
+//Part 4
+
+print(fruitNames)
+
+//Part 5
+
+/*desired result:
+ I like to eat Apple
+ I like to eat Mango
+ I like to eat Peach
+
+ */
+
+//experiment with interpolation
+print("I like to eat \(fruitNames)")
+
+//Part 6
+
+for fruit in fruitNames
+{
+    print("I like to eat " + fruit)
+}
+
+//Part 7 Array Indexes
+
+print(fruitNames[1])
+
+//print item at index 0
+
+
